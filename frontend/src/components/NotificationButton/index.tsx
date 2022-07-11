10b1f117a6d1c180028594d7522591224bdc041a
Renan Soares <renansoaresffc@gmail.com>
2022-07-11 16:12:35 -0300

    feat (frontend): notificationButton component

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,12 @@
+import icon from '../../assets/images/notification-icon.svg';
+
+function NotificationButton() {
+    return (
+        <div className="dsmeta-red-btn">
+            <img src={icon} alt="Notificar" />
+        </div>
+
+    )
+}
+
+export default NotificationButton;
